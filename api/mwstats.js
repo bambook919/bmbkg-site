35ec1d5719ed825d491f65cf6dd84bc54415e7b9
bambook919 <san-francisco.ff@yandex.ru>
2025-04-24 12:01:37 +0500

    Update mwstats.js

--- api/mwstats.js
+++ api/mwstats.js
@@ -1,41 +1,13 @@
-const mongo = require('mongoose');
-    mongo.connect('mongodb+srv://junytwork:ADMIN1@mnstr.wiubncu.mongodb.net/general?retryWrites=true&w=majority&appName=MNSTR', { useUnifiedTopology: true, useNewUrlParser: true });
-
-    const Player = mongo.model('Player',
-        new mongo.Schema({
-            id: Number,
-            login: String,
-            password: String,
-            data: Object,
-            enemyData: Object,
-            regDate: Number,
-            ban: Boolean,
-            banTop: Boolean,
-            authDate: Number,
-            apiToken: String,
-            friends: Array,
-            isAdmin: Boolean,
-            tradeCode: String,
-            tradeRequests: Array,
-            tradesComplete: Array,
-            ipAddress: String,
-            dailyQuestUntil: Number,
-            weeklyQuestUntil: Number,
-            banPromo: Boolean,
-            tg: Number,
-            loginDays: Number,
-            nextBonusDate: Number,
-            nickname: String,
-            vkplay: String
-        })
-    );
+const clientPromise = require('@lib/mongodb');
 
 export async function GET(request) {
+    const client = await clientPromise();
+    const col = client.db('general').collection('players');
     /*await fetch("http://91.245.227.195:1518/mwstats").then(async res => {
         return new Response.json(await res.json());
     }).catch(() => { return new Response.json({ success: false }) });*/
     
-    const players1 = (await Player.find({ })).filter(x=> Object.keys(x.data || {}).includes('atk'));
+    const players1 = (await col.find({ })).filter(x=> Object.keys(x.data || {}).includes('atk'));
     const pt = players1.map(x=> x.data.totalPlaytime || 0).reduce((partialSum, a) => partialSum + a, 0);
     const g = players1.map(x=> x.data.gold || 0).reduce((partialSum, a) => partialSum + a, 0);
     const oa = players1.map(x=> (x.data.atk * x.data.atkMult * (x.data.critChance < 1 ? 1 : 3))).reduce((partialSum, a) => partialSum + a, 0);
