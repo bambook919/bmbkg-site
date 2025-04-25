cca5c14b6f2276d4997d467d0e3b2b1d3c0b292e
bambook919 <san-francisco.ff@yandex.ru>
2025-04-26 02:30:33 +0500

    Update mwstats.en.js

--- js/mwstats.en.js
+++ js/mwstats.en.js
@@ -13,15 +13,15 @@ const utils = {
 }
 
 function SetStats(data) {
-    if(data.data.players && Number.isNaN(data.data.players) == false) document.getElementById('players-count').innerText = (+data.data.players || 0).toLocaleString('ru');
-    if(data.data.playtime && Number.isNaN(data.data.playtime) == false) document.getElementById('playtime-count').innerText = (+data.data.playtime || 0).toLocaleString('ru');
-    if(data.data.kills && Number.isNaN(data.data.kills) == false) document.getElementById('kills-count').innerText = (+data.data.kills || 0).toLocaleString('ru');
-    if(data.data.ascension && Number.isNaN(data.data.ascension) == false) document.getElementById('ascension-count').innerText = (+data.data.ascension || 0).toLocaleString('ru');
+    if(data.data.players && Number.isNaN(data.data.players) == false) document.getElementById('players-count').innerText = (+data.data.players || 0).toLocaleString('en');
+    if(data.data.playtime && Number.isNaN(data.data.playtime) == false) document.getElementById('playtime-count').innerText = (+data.data.playtime || 0).toLocaleString('en');
+    if(data.data.kills && Number.isNaN(data.data.kills) == false) document.getElementById('kills-count').innerText = (+data.data.kills || 0).toLocaleString('en');
+    if(data.data.ascension && Number.isNaN(data.data.ascension) == false) document.getElementById('ascension-count').innerText = (+data.data.ascension || 0).toLocaleString('en');
     if(data.data.gold && Number.isNaN(data.data.gold) == false) document.getElementById('gold-count').innerText = utils.rn(+data.data.gold || 0);
     if(data.data.overallAtk && Number.isNaN(data.data.overallAtk) == false) document.getElementById('overallatk-count').innerText = utils.rn(+data.data.overallAtk || 0);
     data.data.leaderboard.forEach((t, i) => {
         document.getElementById(`lb${i}-login-text`).innerText = t.login;
-        document.getElementById(`lb${i}-ascension-text`).innerHTML = `ASCENSION #${t.ascension.toLocaleString('ru')}<br>ENEMIES KILLED: ${(t.totalKills || 0).toLocaleString('ru')}`;
+        document.getElementById(`lb${i}-ascension-text`).innerHTML = `ASCENSION #${t.ascension.toLocaleString('ru')}<br>ENEMIES KILLED: ${(t.totalKills || 0).toLocaleString('en')}`;
     });
     document.getElementById('serverstatus').innerText = "Server Status: OK";
 }
